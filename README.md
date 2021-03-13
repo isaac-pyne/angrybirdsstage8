8fcca75a2229b586d45254d2362b3da9763e12d3
isaac-pyne <popcorngames2008@gmail.com>
2021-03-13 16:00:32 -0700

    scoring system and dynamic background

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# AngryBirdsStage7
+# AngryBirdsStage8
+use an API call to change the background of the game depending on the time of the day
+at a specified place.  build the scoring system in the game.
+
--- sketch.js
+++ sketch.js
@@ -9,9 +9,10 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
-
+var bg;
+var score = 0;
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackground()
 }
 
 function setup(){
@@ -42,21 +43,34 @@ function setup(){
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
+
+    
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg){
+        background(backgroundImg);
+        noStroke();
+        textSize(35);
+        fill("white");
+        text("Score: " + score, width - 300, 50);
+    }
+ 
     Engine.update(engine);
     //strokeWeight(4);
+    
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
+
     log3.display();
 
     box5.display();
@@ -85,4 +99,24 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBackground(){
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Tokyo");
+    var responseJSON = await response.json();
+    console.log(responseJSON);
+    console.log(responseJSON.datetime);
+
+    var dateTime = responseJSON.datetime;
+    var hour = dateTime.slice(11,13);
+    console.log(hour);
+
+
+    if(hour>=06 && hour<= 19){
+        bg ="sprites/bg.png"
+    } 
+    else{
+        bg = "sprites/bg2.jpg"
+    }
+    backgroundImg = loadImage(bg);
 }
